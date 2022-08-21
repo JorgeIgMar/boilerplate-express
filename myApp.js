7aeb6627381605fd5f06e9c332edc7e2551c1a07
JorgeIgMar <jorgeimsistemas@gmail.com>
2022-08-21 20:27:14 +0200

    Cambios

--- myApp.js
+++ myApp.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
 let express = require('express');
 let app = express();
-//app.get("/",function(req,res){
-  //res.send("Hello Express")
-//});
+app.get("/",function(req,res){
+  res.send("Hello Express")
+});
 
 var response = "Hello json";
-if (process.env.MESSAGE_STYLE) {
+if (process.env.MESSAGE_STYLE === "uppercase") {
   response = "Hello json".toUpperCase();
+}else{
+    response="Hello json";
 }
 app.get("/json",(req, res)=>{res.json({"message":response})});
 
